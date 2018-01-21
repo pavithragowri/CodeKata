033c09d48bf800a94879578f191038a56c2347ce
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-01-21 10:18:55 +0530

    Create prime numbers .c

---
+++ prime numbers .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int low ,high,i,flag;
+  printf("Enter the two numbers (intervals):");
+  scanf("%d%d",&low,&high);
+  while(low<high)
+  {
+    flag=0;
+     for(i=2;i<=low/2;i++)
+     {
+      if(low%i==0)
+      {
+       flag=1;
+       break;
+       }
+     }
+  }
+ }
