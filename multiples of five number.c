7752e41160332f59d18e16d1bf42c925bafbffc2
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-01-21 11:08:26 +0530

    Create multiples of five number.c

---
+++ multiples of five number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ { 
+   longint sum=0,i,n;
+    prinf("given the value of N:);
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+      printf("5*%d=%d",i,5*;);
+     }
+     getch();
+  }
