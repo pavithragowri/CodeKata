e55e6276a849e591c2d0166d9caa0a60765d7a37
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-01-24 16:38:05 +0530

    Create sorted array.c

---
+++ sorted array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int array[MAXSIZE];
+    int num;
+    printf("Enter the value of num \n");
+    scanf("%d", &n);
+    printf("Enter the elements one by one \n");
+    for (i = 0; i < num; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+   
+    printf("Sorted array is...\n");
+    for (i = 0; i < num; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+}
+ 
