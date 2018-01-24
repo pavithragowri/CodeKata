d082dbad623e2c9e057d60e797c2804b0ffb213c
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-01-24 16:45:01 +0530

    Create sortest array.c

---
+++ sortest array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h> 
+int main()
+{
+   int a[30], i, num, sortest;
+   printf("\nEnter no of elements :");
+   scanf("%d", &num);
+   for (i = 0; i < num; i++)
+   scanf("%d", &a[i]);
+   sortest= a[0];
+   for (i = 0; i < num; i++)
+   {
+      if (a[i] < sortest)
+      {
+         sortest = a[i];
+      }
+  }
+   printf("\nSortest Element : %d", sortest);
+   return (0);
+}
