5381a2769b72e799c3603f14815c99050d3b37d2
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-02-09 10:42:50 +0000

    Create SPACE 33.C

---
+++ SPACE 33.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+        char str[100];
+        int i,blank=0;
+        printf("enter the string");
+        scanf("%s",&str[i]);
+         while(str[i]!='\0')
+{
+        if(!((str[i]>='a')&&(str[i]<='z')))
+{
+   blank=blank+1;
+}
+   i++;
+}
+   printf("%d",blank);
+return 0;
+}
