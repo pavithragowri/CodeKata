9215331a410306f495f0edc5c66098c16f87c6f1
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-02-19 16:33:12 +0530

    Create string 41.c

---
+++ string 41.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a,i;
+  char b[10];
+   for(i=0;i<=7;i++)
+  {
+   scanf("%c",&b[i]);
+  }
+   scanf("%d",&a);
+   for(i=0;i<7;i++)
+  {
+    printf("%s",b);
+  }
+return 0;
+}
