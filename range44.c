c5049a64535b37f5436c8cfcd1de7bab57b77160
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-02-21 10:42:07 +0000

    Create range44.c

---
+++ range44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main()
+{
+  int a,count=0;
+  printf("enter the number");
+  scanf("%d",&a);
+  if((a>=1)&&(a<=10))
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+return 0;
+}
