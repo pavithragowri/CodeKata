410ad74fbd83e3d83b99ba4556747fbbc493baf2
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-03-02 15:52:14 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b;
+   printf("enter the number");
+   scanf("%d%d",&a,&b);
+   a=a^b;
+   b=b^a;
+   a=a^b;
+   printf("enter the value of a&b after a swapping");
+return 0;
+}
