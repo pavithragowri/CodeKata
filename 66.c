0c71e15877793823e300c659f0bbb7cce206ba5d
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-03-14 10:46:35 +0000

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+       printf("prime number");
+    }
+    else
+    {
+        printf("not prime number");
+    }
+    return 0;
+}
+
