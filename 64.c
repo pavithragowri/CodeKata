ec0671c14bd2a0cd709ef3120b73336e7fb5f7d5
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2018-03-14 10:34:12 +0000

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the value");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("odd");
+    }
+    else
+    {
+        printf("even");
+    }
+return 0;    
+}
+
